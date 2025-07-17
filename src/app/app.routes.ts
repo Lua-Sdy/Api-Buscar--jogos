b6adbc2097d6ae2f68cfa259313eb117619242f8
luã Oliveira <luan199903@gmail.com>
2025-07-17 11:53:39 -0300

    Adiciomando implementos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
 import { GameSearchComponent } from './components/game-search/game-search';
 import { GameDetailsComponent } from './components/game-details/game-details';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { GameSearch } from './pages/game-search/game-search';
 
 export const routes: Routes = [
     { path: '', component: GameSearchComponent },
     { path: 'game/:slug', component: GameDetailsComponent },
+
+    {
+        path:"",
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/login/login').then(c => Login )
+    },
+     {
+        path:"home",
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/home/home').then(c => Home)
+    },
+     {
+        path:"game-search",
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/game-search/game-search').then(c => GameSearch )
+    }
 ];
