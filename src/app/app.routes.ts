0c761dfdbea06a3d43de661f001640064b2444d4
luã Oliveira <luan199903@gmail.com>
2025-07-18 11:41:38 -0300

    implementada telas inicias não/finalizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,19 @@ import { GameDetailsComponent } from './components/game-details/game-details';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
 import { GameSearch } from './pages/game-search/game-search';
+import { Cadastros } from './pages/cadastros/cadastros';
 
 export const routes: Routes = [
-    { path: '', component: GameSearchComponent },
-    { path: 'game/:slug', component: GameDetailsComponent },
-
+    
     {
         path:"",
         pathMatch: 'full',
         loadComponent: () => import('./pages/login/login').then(c => Login )
+    },
+    {
+        path:"cadastro",
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/cadastros/cadastros').then(c => Cadastros) 
     },
      {
         path:"home",
---
+++ src/app/pages/cadastros/cadastros.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CadastroForm } from "../../components/cadastro-form/cadastro-form";
+
+@Component({
+  selector: 'app-cadastros',
+  imports: [CadastroForm],
+  templateUrl: './cadastros.html',
+  styleUrl: './cadastros.css'
+})
+export class Cadastros {
+
+}
